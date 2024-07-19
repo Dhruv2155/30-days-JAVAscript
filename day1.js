a37f10e97dfac7c81d0023ccbc0b7a7ffc98c8db
Dhruv2155 <dhruvwsh@gmail.com>
2024-07-19 13:55:38 +0530

    day 1 tasks completed

---
+++ day1.js
@@ -0,0 +1,86 @@
+// ACtivity 1
+    // task 1:
+    var a = 23;
+    console.log(a)
+
+    // task 2:
+    let str = "Thanos"
+    console.log(str)
+
+
+
+// Activty 2:
+    // task 3:
+        const boo = true
+        console.log(boo)
+
+
+// Activity 3:
+    // task 4:
+    var num = 7
+    var str1 = "thala for a reason"
+    var Abkibaar400PAAR = false;
+    const arr = ["elvish","Bhaaaaaaaiiiii"]
+    const obj = new Object()
+
+    console.log(typeof(num))
+    console.log(typeof(str1))
+    console.log(typeof(Abkibaar400PAAR))
+    console.log(typeof(arr))
+    console.log(typeof(obj))
+
+ 
+
+//  Actvity 4:
+    // task 5:
+    let b = "Arvind KejriWaal"
+    console.log(b)
+    b = "daarubaaj"
+    console.log(b)
+
+
+// Activity 5:
+    // task 5:
+       const pm = "Narendra Modi"
+       console.log(pm)
+        // pm = "Rahul Gandhi" <-|
+        // error causing line ___|
+
+   
+        var num1 = 99
+        var str2 = "haarne ka bhi ghamand hai (Khangress)"
+        var  aglibaarINDIAsarkaar = false;
+        const arr1 = ["hello I am under the water "]
+        const obj2= new Object()
+         let mindinwomen;
+         let richPeopleinPak = null;
+
+    console.log(num1,typeof(num1))
+    console.log(str2,typeof(str2))
+    console.log(aglibaarINDIAsarkaar,typeof(aglibaarINDIAsarkaar))
+    console.log(arr1,typeof(arr1))
+    console.log(obj2,typeof(obj2))
+    console.log(mindinwomen,typeof(mindinwomen))
+    console.log(richPeopleinPak,typeof(richPeopleinPak))
+
+
+    let ex = "jaan"
+    console.log(ex)
+    ex = "naagin"
+    console.log(ex)
+
+
+    const PM = "Narendra Modi"
+    console.log(PM)
+    
+    
+    // const PM = "Rahul Gandhi"
+    // error causing line
+
+
+
+
+
+
+
+
